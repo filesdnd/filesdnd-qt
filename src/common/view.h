cd96a0c1de6ce2e6a98c19e5e2bdd0136c282cef
Kévin Renella <kevin.renella@gmail.com>
2014-08-12 20:46:07 +0200

    Fix Travis-ci build

--- src/common/view.h
+++ src/common/view.h
@@ -2,11 +2,11 @@
 #define COMMON_VIEW_H
 
 #if defined(Q_OS_LINUX)
-#include "linux-ui/view.h"
+// #include "linux-ui/view.h"
 #elif defined(Q_OS_WIN)
 #include "windows-ui/view.h"
 #elif defined(Q_OS_OSX)
-#include "mac-ui/view.h"
+// #include "mac-ui/view.h"
 #endif
 
 #endif // COMMON_VIEW_H
